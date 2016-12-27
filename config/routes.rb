ddebcb2ae7b0ef1cc82074f760479906174df30f
Rails Udemy Course <infosubnetting@gmail.com>
2016-12-27 19:29:32 +0000

    Complete homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'welcome#home'
-  get 'about', to: 'welcome#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   
   resources :articles
 
