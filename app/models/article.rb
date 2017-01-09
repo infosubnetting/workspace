57e387c6f28751a30f9226ca6676fa717e7e51f7
Rails Udemy Course <infosubnetting@gmail.com>
2017-01-09 18:18:10 +0000

    add associations between user anda articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ActiveRecord::Base
   
   # rendo obbligatori i dati dei campi :title e :description per riempire la 
   # tabella
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximun: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
-  
+  validates :user_id, presence: true
 end
